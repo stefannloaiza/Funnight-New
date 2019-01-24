4b346ecb8d1f55eab15a5bb58ab47bd39abf4799
StefannLoaiza <stefannloa@gmail.com>
2019-01-23 20:52:44 -0500

    Commit Ver.1.1.2
    Cambios pequenos. Se cambia fondo y pantalla de welcome.
    
    Saludos.

--- Funnight/resources/views/welcome.blade.php
+++ Funnight/resources/views/welcome.blade.php
@@ -1,92 +1,124 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>FUNNIGHT</title>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <link href="{{ asset('css/welcome.css') }}" rel="stylesheet" type="text/css">
+    <title>FUNNIGHT</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/welcome.css') }}" rel="stylesheet" type="text/css">
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Styles -->
+    <style>
+        html,
+        body {
+            background-image: url("img/fondo2.jpg");
+            background-attachment: fixed;
+            background-repeat: no-repeat;
+            background-size: 100%;
+            background-color: #fff;
+            color: white;
+            font-weight: 100;
+            /* height: 100vh; */
+            margin: 0;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .full-height {
+            height: 100vh;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 30px;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        .content {
+            text-align: center;
+            margin: 15%;
+        }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+        .title {
+            font-size: 84px;
+        }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">{{ __('welcome.home') }}</a>
-                    @else
-                        <a href="{{ route('login') }}">{{ __('welcome.login') }}</a>
-                        <a href="{{ route('register') }}">{{ __('welcome.register') }}</a>
-                    @endauth
-                </div>
-            @endif
+        .links>a {
+            color: white;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
 
-            <div class="content">
-                <div class="title m-b-md">
-                    <h1 class="display-1">Fun Night</h1>
-                </div>
-                <h2>
-                    Disfruta la noche de otra manera. Promociones y eventos cada noche. Para que pases una noche divertida.
-                </h2>
-                <a class="btn btn-primary" href="{{ route('register') }}" role="button">Registrate</a>
-            </div>
+        .links>a:hover {
+            color: #23C8F9;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+
+        #logo {
+            width: 5%;
+            margin: 1%;
+        }
+
+        .lead{
+
+        }
+    </style>
+</head>
+
+<body>
+    <div class="position-ref">
+        <div class="logo">
+            <img src="img/logov2.png" class="img-rounded" id="logo" alt="">
         </div>
-    </body>
+        @if (Route::has('login'))
+            <div class="top-right links">
+            @auth
+                <a href="{{ url('/home') }}">{{ __('welcome.home') }}</a> 
+        @else
+                <a href="{{ route('login') }}">{{ __('welcome.login') }}</a>
+                <a href="{{ route('register') }}">{{ __('welcome.register') }}</a> 
+            @endauth
+            </div>
+        @endif
+
+        <div class="content">
+            <div class="title">
+                <p>
+                    <b>
+                        Fun Night
+                    </b>
+                </p>
+            </div>
+            <br>
+            <p class="lead">
+                Fun Night te ayuda a encontrar tu sitio nocturno favorito, registrate como usuario para encuentres tu sitio ideal o resgistra
+                tu establecimiento nocturno, haz parte de esta comunidad y comienza a crecer tu negocio.
+            </p>
+            <br>
+            <a class="btn btn-primary btn-lg" href="{{ route('register') }}" role="button">Registrate</a>
+        </div>
+    </div>
+</body>
+
 </html>
