60937e5d48d8f05f7d1f4702dd72401546f8ccb7
christian sanchez <christiansq2018@gmail.com>
2019-01-30 19:48:52 -0500

    ultimo cambio 7:50p.m

--- Funnight/resources/views/user/profile.blade.php
+++ Funnight/resources/views/user/profile.blade.php
@@ -17,25 +17,13 @@
                     <h2>{{$user->name.' '.$user->surname}}</h2>
                     <p>{{'Se unio: '.\FormatTime::LongTimeFilter($user->created_at)}}</p>
 
-<<<<<<< HEAD
-                    @if( $user->hasRole('site') )
-                    <button name="" id="" type="button" class="btn btn-success">
-                            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Seguir
-                        </button> {{-- <button name="" id="" type="button" class="btn btn-primary">
-                            <span class="glyphicon glyphicon-heart" aria-hidden="true"></span> Marcar como favorito
-                        </button> --}} @endif
-=======
-                    @if( $user->hasRole('site') ) 
-                        @if( Auth::user()->hasRole('site') )
-                            <button name="followSite" id="{{ $user->id }}" type="button" class="btn btn-primary btn-block followSite">
+                    @if( $user->hasRole('site') ) @if( Auth::user()->hasRole('site') )
+                    <button name="followSite" id="{{ $user->id }}" type="button" class="btn btn-primary btn-block followSite">
                                 Seguir
                             </button>
-                            <button name="unfollowSite" id="{{ $user->id }}" type="button" class="btn btn-success btn-block unfollowSite" hidden>
+                    <button name="unfollowSite" id="{{ $user->id }}" type="button" class="btn btn-success btn-block unfollowSite" hidden>
                                 <span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Seguido
-                            </button>
-                        @endif
-                    @endif
->>>>>>> e1780b4cb999ee624de62059de8f7732d420d21c
+                            </button> @endif @endif
                 </div>
 
                 {{-- Calificacion por estrellas 1--}}
@@ -71,16 +59,13 @@
             </div>
             <div class="clearfix"></div>
             <div class="images_profile">
-<<<<<<< HEAD
-                {{--
-                <h2>Publicaciones</h2> --}}
-=======
-                <h2>Publicaciones</h2>
->>>>>>> e1780b4cb999ee624de62059de8f7732d420d21c
-                <hr> @if ($user->images != null && $user->images != "") @foreach ($user->images as $image)
+                <<<<<<< HEAD {{-- <h2>Publicaciones</h2> --}} =======
+                    <h2>Publicaciones</h2>
+                    >>>>>>> e1780b4cb999ee624de62059de8f7732d420d21c
+                    <hr> @if ($user->images != null && $user->images != "") @foreach ($user->images as $image)
     @include('includes.image',['image'=>$image])
-                @endforeach @else
-                <small>Este usuario no tiene publicaciones.</small> @endif
+                    @endforeach @else
+                    <small>Este usuario no tiene publicaciones.</small> @endif
             </div>
         </div>
     </div>
