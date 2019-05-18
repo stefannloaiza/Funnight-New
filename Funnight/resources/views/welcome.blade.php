de19190031943d147a5ca153a9cf2c899ac0664a
StefannLoaiza <stefannloa@gmail.com>
2019-05-18 00:34:14 -0500

    Commit Ver. 1.12.1
    Se mejora design de la app. se permiten mejoras en las vistas del movil y tablet.

--- Funnight/resources/views/welcome.blade.php
+++ Funnight/resources/views/welcome.blade.php
@@ -1,128 +1,130 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>FUNNIGHT</title>
+        <title>FUNNIGHT</title>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('css/welcome.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('css/welcome.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
-    <!-- Styles -->
-    <style>
-        html,
-        body {
-            background-image: url("img/fondo2.jpg");
-            background-attachment: fixed;
-            background-repeat: no-repeat;
-            background-size: 100%;
-            background-color: #fff;
-            color: white;
-            font-weight: 100;
-            /* height: 100vh; */
-            margin: 0;
-        }
+        <!-- Styles -->
+        <style>
+            html,
+            body {
+                background-image: url("img/fondo2.jpg");
+                background-attachment: fixed;
+                background-repeat: no-repeat;
+                background-size: 100% 100%;
+                background-color: #fff;
+                color: white;
+                font-weight: 100;
+                /* height: 100vh; */
+                margin: 0;
+            }
 
-        .full-height {
-            height: 100vh;
-        }
+            .full-height {
+                height: 100vh;
+            }
 
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
 
-        .position-ref {
-            position: relative;
-        }
+            .position-ref {
+                position: relative;
+            }
 
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 30px;
-        }
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 30px;
+            }
 
-        .content {
-            text-align: center;
-            margin: 10em;
-            margin-bottom: 0;
-        }
+            .content {
+                text-align: center;
+                margin: 10em;
+                margin-bottom: 0;
+            }
 
-        .title {
-            font-size: 84px;
-        }
+            .title {
+                font-size: 84px;
+            }
 
-        .links>a {
-            color: white;
-            padding: 0 25px;
-            font-size: 12px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
+            .links>a {
+                color: white;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
 
-        .links>a:hover {
-            color: #23C8F9;
-        }
+            .links>a:hover {
+                color: #23C8F9;
+            }
 
-        .m-b-md {
-            margin-bottom: 30px;
-        }
+            .m-b-md {
+                margin-bottom: 30px;
+            }
 
-        #logo {
-            width: 5%;
-            margin: 1%;
-        }
+            #logo {
+                width: 5%;
+                margin: 1%;
+            }
 
-        .lead {}
-    </style>
-</head>
+            .lead {}
 
-<body>
-    <div class="position-ref">
-        <div class="logo">
-            <img src="img/logov2.png" class="img-rounded" id="logo" alt="">
-        </div>
-        @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-            <a href="{{ url('/home') }}">{{ __('welcome.home') }}</a> @else
-            <a href="{{ route('login') }}">{{ __('welcome.login') }}</a>
-            <a href="{{ route('register') }}">{{ __('welcome.register') }}</a> @endauth
-        </div>
-        @endif @auth
-        <div class="container">
-            <div class="alert alert-success text-center" role="alert">
-                <h2>
-                    Bienvenido {{ ucwords( Auth::user()->name ) }}!!
-                </h2>
-                Continua en el Home...
+        </style>
+    </head>
+
+    <body>
+        <div class="position-ref">
+            <div class="logo">
+                <img src="img/logov2.png" class="img-rounded" id="logo" alt="">
+            </div>
+            @if (Route::has('login'))
+            <div class="top-right links">
+                @auth
+                <a href="{{ url('/home') }}">{{ __('welcome.home') }}</a> @else
+                <a href="{{ route('login') }}">{{ __('welcome.login') }}</a>
+                <a href="{{ route('register') }}">{{ __('welcome.register') }}</a> @endauth
+            </div>
+            @endif @auth
+            <div class="container">
+                <div class="alert alert-success text-center" role="alert">
+                    <h2>
+                        Bienvenido {{ ucwords( Auth::user()->name ) }}!!
+                    </h2>
+                    Continua en el Home...
+                </div>
+            </div>
+            @endauth
+            <div class="content">
+
+                <div class="title"><b>
+                        Fun Night
+                    </b>
+                </div>
+                <br>
+                <p class="lead">
+                    Fun Night te ayuda a encontrar tu sitio nocturno favorito, registrate como usuario para encuentres
+                    tu sitio ideal o resgistra
+                    tu establecimiento nocturno, haz parte de esta comunidad y comienza a crecer tu negocio.
+                </p>
+                <br> @guest
+                <a class="btn btn-primary btn-lg" href="{{ route('register') }}" role="button">Registrate</a> @endguest
             </div>
         </div>
-        @endauth
-        <div class="content">
-
-            <div class="title"><b>
-                Fun Night
-            </b>
-            </div>
-            <br>
-            <p class="lead">
-                Fun Night te ayuda a encontrar tu sitio nocturno favorito, registrate como usuario para encuentres tu sitio ideal o resgistra
-                tu establecimiento nocturno, haz parte de esta comunidad y comienza a crecer tu negocio.
-            </p>
-            <br> @guest
-            <a class="btn btn-primary btn-lg" href="{{ route('register') }}" role="button">Registrate</a> @endguest
-        </div>
-    </div>
-</body>
+    </body>
 
 </html>
